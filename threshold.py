2c26ea5d0ebb70b095a4540d19b41559ab10410a
Sema <168088544+semayilmazs@users.noreply.github.com>
2024-12-18 16:30:36 +0300

    Update threshold.py

--- threshold.py
+++ threshold.py
@@ -8,7 +8,7 @@ import numpy as np
 
 print("Current Working Directory:", os.getcwd())
 # Change the dataset for your computer
-dataset_path = os.path.join(r"C:\Users\LENOVO\OneDrive\Masaüstü\güz24-25\scientific\ScientificProject\dataset_for_project", "*.png")
+dataset_path = os.path.join("dataset_for_project", "*.png")
 image_paths = glob.glob(dataset_path)
 print(f"Number of images found: {len(image_paths)}")
 
